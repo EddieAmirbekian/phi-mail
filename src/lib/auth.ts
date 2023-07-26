2fd6ee792b82d0e663036c0e116ed3230cd39cb2
Eduard <eduard.amirbekyan@volo.global>
2023-07-26 14:18:18 +0400

    remove facebook

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -19,10 +19,6 @@ export const authOptions: NextAuthOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-    }),
-    FacebookProvider({
-      clientId: process.env.FACEBOOK_CLIENT_ID!,
-      clientSecret: process.env.FACEBOOK_CLIENT_SECRET!
     })
   ],
   callbacks: {
